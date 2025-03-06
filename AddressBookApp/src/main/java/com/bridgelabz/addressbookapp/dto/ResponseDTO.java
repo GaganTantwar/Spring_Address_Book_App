88752219d5f5a30f34dc2d3b45f80a36d16bf7e7
GaganTantwar <panthigagan478@gmail.com>
2025-03-06 16:49:06 +0530

    Gagan Tantwar Extended feat : Use Lombok Library to auto generate getters and setters for the DTO

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.addressbookapp.dto;
+import lombok.Data;
 
-public class ResponseDTO {
+public @Data class ResponseDTO {
     private String message;
     private Object data;
     public ResponseDTO(String message,Object data){
