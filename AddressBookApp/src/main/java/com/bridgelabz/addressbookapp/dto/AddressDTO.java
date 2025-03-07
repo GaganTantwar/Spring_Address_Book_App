0a1186ab594fd6dae8f313bf627c63447ce5f647
GaganTantwar <panthigagan478@gmail.com>
2025-03-07 11:10:48 +0530

    Gagan Tantwar Extended feat :Ensure validations is done on the AddressBook DTO

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressDTO.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressDTO.java
@@ -7,10 +7,16 @@ public class AddressDTO {
     @NotEmpty(message = "Name Should not be empty")
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = " Name Should be Valid")
     public String name;
-    @NotEmpty(message = "Addres Should not Empty")
+    @NotEmpty(message = "Address Should not be empty")
     public String address;
+    @NotEmpty(message = "City Should not be Empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "City Should be Valid")
     public String city;
+    @NotEmpty(message = "State should not be empty")
+    @Pattern(regexp = "^[a-zA-Z]+\\s[a-zA-Z]+$",message = "State Should be valid")
     public String state;
+    @NotEmpty(message = "Zip Code should not be empty")
+    @Pattern(regexp = "\\d{6}" ,message = "Zip Code should be valid")
     public String zipcode;
 
 }
--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/exception/AddressBookAppCustomeExceptionHandler.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/exception/AddressBookAppCustomeExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapp.exception;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,8 +9,17 @@ import com.bridgelabz.addressbookapp.dto.ResponseDTO;
 import java.util.*;
 import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
+@Slf4j
 public class AddressBookAppCustomeExceptionHandler {
+    private static final String message="Exception while Processing REST Request";
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO>handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid Date Format",exception);
+        ResponseDTO responseDTO=new ResponseDTO(message,"Should have date in Format dd MM yyyy");
+        return  new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
         List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
@@ -18,7 +28,7 @@ public class AddressBookAppCustomeExceptionHandler {
         return  new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(AddressBookException.class)
-    public ResponseEntity<ResponseDTO> handleEmployeePayRollException(AddressBookException exception){
+    public ResponseEntity<ResponseDTO> handleAddressBookException(AddressBookException exception){
         ResponseDTO responseDTO=new ResponseDTO("Exception while processing REST Request",exception.getMessage());
         return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
     }
