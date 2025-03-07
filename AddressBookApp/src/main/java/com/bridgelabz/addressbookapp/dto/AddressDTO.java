253fee1e0f801b22c97055f0212a2fc1610b50e9
GaganTantwar <panthigagan478@gmail.com>
2025-03-07 10:32:10 +0530

    Gagan Tantwar Added feat : Add remaining properties to the Address Book DTO and Model

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressDTO.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressDTO.java
@@ -2,19 +2,15 @@ package com.bridgelabz.addressbookapp.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-
+import lombok.ToString;
 public class AddressDTO {
     @NotEmpty(message = "Name Should not be empty")
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = " Name Should be Valid")
     public String name;
     @NotEmpty(message = "Addres Should not Empty")
     public String address;
-    public AddressDTO(String name,String address){
-        this.name=name;
-        this.address=address;
-    }
-    @Override
-    public String toString(){
-        return "Address "+name +" "+address+" ";
-    }
+    public String city;
+    public String state;
+    public String zipcode;
+
 }
--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -1,38 +1,22 @@
 package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.AddressDTO;
-
-public class AddressBookData {
+import lombok.Data;
+public @Data class AddressBookData {
     private long phone;
     private String name;
     private String address;
+    private String city;
+    private String state;
+    private String zipcode;
     public AddressBookData(){}
     public AddressBookData(long phone, AddressDTO addressDTO){
         this.phone=phone;
         this.name=addressDTO.name;
         this.address=addressDTO.address;
-    }
-    public long getPhone(){
-        return phone;
-    }
-    public void setPhone(long phone){
-        this.phone=phone;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getAddress(){
-        return address;
-    }
-    public void setAddress(String address){
-        this.address=address;
-    }
-    @Override
-    public String toString(){
-        return "Address "+phone+" "+name+" "+" "+address;
+        this.state=addressDTO.state;
+        this.city=addressDTO.city;
+        this.zipcode=addressDTO.zipcode;
     }
 }
 
